c4780a4e2a170a0834ef22662e35ae4856fd455b
blastorp <jernestm@gmail.com>
2024-09-21 21:56:02 -0600

    creacion Clase producto

---
+++ MarketPlaceGit/Models/Producto.cs
@@ -0,0 +1,6 @@
+﻿namespace MarketPlaceGit.Models
+{
+    public class Producto
+    {
+    }
+}
--- MarketPlaceGit/obj/Debug/net6.0/MarketPlaceGit.AssemblyInfo.cs
+++ MarketPlaceGit/obj/Debug/net6.0/MarketPlaceGit.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketPlaceGit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bdca422e08fd7098f55792e524ec38e5bbe0396")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketPlaceGit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketPlaceGit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
